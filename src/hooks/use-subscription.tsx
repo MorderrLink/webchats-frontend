bd12574cd4360d82d1a1da329782c5082f2b8cf2
MorderrLink <burmenskiy90@bk.ru>
2025-01-24 16:59:44 +0300

    corrections with hrefs 2

--- src/hooks/use-subscription.tsx
+++ src/hooks/use-subscription.tsx
@@ -31,7 +31,7 @@ export const UseSubscription = () => {
             userId: user?.id 
           };
           
-          await fetch(`https://${process.env.BACKEND_ENDPOINT}/api/save-subscription`, {
+          await fetch(`https://${process.env.BACKEND_ENDPOINT!}/api/save-subscription`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(subscriptionData),
